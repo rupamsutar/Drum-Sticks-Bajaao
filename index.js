9d174e7c7457027f7c8ad9806591f433d88d7d62
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-05 21:04:28 +0530

    addEventListener Added to all the buttons

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.querySelector("button").addEventListener("click", handleClick);
+
+
+
+for (var i = 0; i < (document.querySelectorAll(".drum").length); i ++ ) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+function handleClick() {
+  alert("I got clicked !");
+}
