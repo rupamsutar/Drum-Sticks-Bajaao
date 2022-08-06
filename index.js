8205ebec641a00c05b0a923f54a3843d0fbc10c8
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-06 10:48:49 +0530

    1 Sound added and background images added

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-
-document.querySelector("button").addEventListener("click", handleClick);
-
-
-
 for (var i = 0; i < (document.querySelectorAll(".drum").length); i ++ ) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
 function handleClick() {
-  alert("I got clicked !");
+  var audio = new Audio('sounds/tom-1.mp3');
+  audio.play();
 }
